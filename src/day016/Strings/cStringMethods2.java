2e3c3336e1b8394b873ce57ec214c3702a220614
AlkanTuncer <alkantuncer53@hotmail.com>
2022-03-22 21:13:15 +0300

    3.Hafta Array - 2D Array - Jagged Array - 3D Array Strings - Strings Methods

--- src/day016/Strings/cStringMethods2.java
+++ src/day016/Strings/cStringMethods2.java
@@ -11,5 +11,30 @@ public class cStringMethods2 {
 
         str1=str1.replace('ı','i');
         System.out.println(str1);
+
+        System.out.println();
+
+
+        System.out.println("".isEmpty());   //T
+        System.out.println("".isBlank());   //T
+        System.out.println("A".isEmpty());  //F
+        System.out.println("A".isBlank());  //F
+        System.out.println(" ".isEmpty());  //F
+        System.out.println(" ".isBlank());  //T
+        System.out.println("               ".isBlank()); //T
+
+        String str17="     Bu bir deneme mesajıdır.     ";
+        String str23="Bu bir deneme mesajıdır.";
+        System.out.println(str17);
+        System.out.println(str23);
+
+        System.out.println("str17 : "+str17.length());
+        System.out.println("str23 : "+str23.length());
+
+        String str20=str17.trim();  // Bir metnin sagında ve solunda bosluk karakteri var ise trim metotu o boslukları kaldırır ve yeni nesne olarak başka değişkene atarız.
+        System.out.println(str20);                                                                                             // orjinal nesnedeki metin değişmez.
+        System.out.println("str20 : "+str20.length());
+        System.out.println(str17);
+
     }
 }
